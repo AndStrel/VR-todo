6ae42dfebc9cd6d616c4f5a269588cf148031014
Andrey <xoz9iih@outlook.com>
2026-04-25 13:21:18 +0300

    открываем страничку при запуске dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/test-todo-list/',
   plugins: [react()],
+  server: {
+    open: true,
+  },
   test: {
     environment: 'jsdom',
     globals: true,

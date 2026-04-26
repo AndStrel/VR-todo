618453ff962fbe20e5359863749a0402a05e6323
Andrey <xoz9iih@outlook.com>
2026-04-26 11:44:01 +0300

    Переименован проект и обновлены пути в конфигурациях.

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173/test-todo-list/',
+    baseUrl: 'http://localhost:5173/VR-todo/',
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: false,
     video: false,
